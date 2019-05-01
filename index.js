29dbed516cefe73382d052ee3f32cb941ee6755e
Nicolas Marini <nmarini44@gmail.com>
2019-05-01 21:39:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,28 +1,28 @@
-class BoardMember {
+class BoardMember{
   constructor(name, homeState, training){
-    this.name = name;
-    this.homeState = homeState;
-    this.training = training;
+    this.name = name
+    this.homeState = homeState
+    this.training = training
   }
   veto(){
-    return `No, I must disagree`
+    return 'No, I must disagree'
   }
   approve(){
-    return `You can do that!`
+    return 'You can do that!'
   }
   doCharity(){
-    return `I like to help people.`
+    return "I like to help people."
   }
   releasePressStatement(){
-    return `You will see great things from Scuber.`
+    return "You will see great things from Scuber."
   }
   sayHi(){
     return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`
   }
 }
 
-class Ceo exteneds BoardMember {
+class Ceo extends BoardMember {
   hireEmployee(){
-    return `Welcome aboard!`
+    return "Welcome aboard!"
   }
 }
